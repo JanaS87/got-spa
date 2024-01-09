5b6366e388ae39d5501ba12598ef7a54ab943026
Jana Schmälzle <136725332+JanaS87@users.noreply.github.com>
2024-01-09 18:57:38 +0100

    Update HomeView.vue
    deleted some text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,15 +7,6 @@
 Take a look at the houses, learn about each character or browse through all the quotes that exist.</h3>
     <p class="fun">Have fun!</p>
     <br><hr><br>
-
-    <p class="explanation">
-      This is my final version for the skill test.
-
-      I hope you like my attempt so far.
-
-      Since Vue is still completely new to me, I had to browse the internet a bit more, but I'm happy with the result and with myself.
-    </p>
-
   </main>
 </template>
 
@@ -47,15 +38,5 @@ hr {
   margin-top: 2rem;
   margin-bottom: 2rem;
   opacity: 0.3;
-}
-
-.explanation {
-  font-size: 1.2rem;
-  font-family: Arial, Helvetica, sans-serif;
-  font-weight: 400;
-  text-align: center;
-  max-width: 30%;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
 }
 </style>
